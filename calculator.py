1f0df1f44a83615be07cae4601774e39e783644b
Bardo <bardoespinosaa@gmail.com>
2023-07-10 11:35:12 -0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,31 +3,35 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Calculator")
 
-ent = tk.Entry(root,  width=35, borderwidth=5)
+ent = tk.Entry(root,  width=35, borderwidth=5, justify="right")
+result = tk.Label(root, text="0", justify="right")
 
 nums = []
 
 def button_click(text):
     val=ent.get()+f"{text}"
     ent.delete(0, tk.END)
-    print(val)
     ent.insert(0, val)
 
-def btn_clear():
+def button_clear():
     ent.delete(0, tk.END)
+    result["text"]="0"
     global nums
+    print(nums)
     nums=[]
 
 def button_add():
-    if ent.get():
-        print(ent.get())
+    global nums
+    print(nums)
+    if ent.get().isdigit():
         nums.append(int(ent.get()))
-        ent.delete(0, tk.END)
-        print(sum(nums) )
+    ent.delete(0, tk.END)
+    result["text"]=sum(nums)
+    nums=[sum(nums)]
 
 button_sum = tk.Button(root, text="+", padx=40, pady=20, command=button_add)
-button_eq = tk.Button(root, text="=", padx=40, pady=20, command=lambda: print(root.winfo_reqwidth()))
-button_clear = tk.Button(root, text="Clear ", padx=125, pady=20, command=btn_clear)
+button_eq = tk.Button(root, text="=", padx=40, pady=20, command=button_add)
+button_clear = tk.Button(root, text="Clear ", padx=125, pady=20, command=button_clear)
 
 
 buttons=[]
@@ -35,26 +39,25 @@ for n in list(range(10)):
     btn = tk.Button(root, text=n, padx=40, pady=20, command=lambda x=n:button_click(x))
     buttons.append(btn)
 
-print(buttons)
-
 ent.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+result.grid(row=1, column=2, columnspan=3)
 
-buttons[7].grid(row=1, column=0)
-buttons[8].grid(row=1, column=1)
-buttons[9].grid(row=1, column=2)
+buttons[7].grid(row=2, column=0)
+buttons[8].grid(row=2, column=1)
+buttons[9].grid(row=2, column=2)
                    
-buttons[4].grid(row=2, column=0)
-buttons[5].grid(row=2, column=1)
-buttons[6].grid(row=2, column=2)
+buttons[4].grid(row=3, column=0)
+buttons[5].grid(row=3, column=1)
+buttons[6].grid(row=3, column=2)
 
-buttons[1].grid(row=3, column=0)
-buttons[2].grid(row=3, column=1)
-buttons[3].grid(row=3, column=2)
+buttons[1].grid(row=4, column=0)
+buttons[2].grid(row=4, column=1)
+buttons[3].grid(row=4, column=2)
 
-buttons[0].grid(row=4, column=0)
-button_sum.grid(row=4, column=1)
-button_eq.grid(row=4, column=2)
+buttons[0].grid(row=5, column=0)
+button_sum.grid(row=5, column=1)
+button_eq.grid(row=5, column=2)
 
-button_clear.grid(row=5, column=0, columnspan=3)
+button_clear.grid(row=6, column=0, columnspan=3)
 
 root.mainloop()
