e472d41ea0b25b9fb0758a8d3263f4204edc192b
Bardo <bardoespinosaa@gmail.com>
2023-07-11 08:11:27 -0600

    PIL deleted

--- radio.py
+++ radio.py
@@ -1,8 +1,9 @@
 import tkinter as tk
-from PIL import ImageTk, Image
+#from PIL import ImageTk, Image #I dont need this
 
 root = tk.Tk()
 root.title("Something something")
+
 r=tk.IntVar()
 r.set("1")
 
